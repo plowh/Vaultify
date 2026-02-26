855d16ca7d3446bd52e7430130f5dde31ff63a0c
Plowh <126720664+Plowh@users.noreply.github.com>
2026-02-26 15:14:01 +0100

    Delete PasswordGenerator directory

--- PasswordGenerator/src/main.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-std::string generatePassword(const int numLowercaseLetters, const int numUppercaseLetters, const int numNumbers, const int numSymbols,
-    const char* lowercaseLetters, const char* uppercaseLetters, const char* numbers, const char* symbols);
-
-void savePassword(std::string currentPassword);
-bool IsInTitleBar(HWND hwnd, LPARAM lParam);
-void saveCustomPassword(std::string customPassword);
